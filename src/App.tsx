64ce6b46bf929e09b7817a3f1c0aafa35edea206
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-05-17 19:30:54 -0700

    update handleSubmit to check if the newly submitted todo is dependent on another todo and update that main todo with the dependents id. create signal for temp storage of todo ids that are wanting to showing dependent. filter main displayed todos off having no dependents. give available options for todos that can be depended on. add get dependent props func

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import type { DOMElement } from "solid-js/jsx-runtime";
-import { TodoComp } from "./components/Todo";
 import { createFakeTodos } from "./helpers/createFakeTodos";
 import { createSignal, For } from "solid-js";
 import { createTodo } from "./helpers/createTodo";
+import type { Todo } from "./types";
+import { TodoSet, type TodoSetProps } from "./components/TodoSet";
 
 type FormEvent = SubmitEvent & {
   currentTarget: HTMLFormElement;
@@ -10,38 +11,92 @@ type FormEvent = SubmitEvent & {
 };
 
 export function App() {
-  const [currentTodos, setCurrentTodos] = createSignal(createFakeTodos(3));
+  const [currentTodos, setCurrentTodos] = createSignal(createFakeTodos(1));
   const [newTodo, setNewTodo] = createSignal("");
-  const [dependsOn, setDependsOn] = createSignal<string>();
+  const [dependsOn, setDependsOn] = createSignal<Todo["id"]>();
+  const [showDependentsForTodo, setShowDependentsForTodo] = createSignal<
+    Record<Todo["id"], boolean>
+  >({});
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
     const todo = createTodo({
       description: newTodo(),
       dependsOn: dependsOn(),
     });
+
+    const dependedOn = currentTodos().find((item) => item.id === dependsOn());
+
+    if (dependedOn) {
+      const updatedMain = { ...dependedOn, dependent: todo.id };
+
+      setCurrentTodos((curTodos) => [
+        ...curTodos.filter((item) => item.id !== dependedOn.id),
+        updatedMain,
+        todo,
+      ]);
+    } else {
+      setCurrentTodos((todos) => [...todos, todo]);
+    }
     setNewTodo("");
     setDependsOn();
-    setCurrentTodos((curTodos) => [...curTodos, todo]);
   }
 
+  function handleShowDependent(id: Todo["id"]) {
+    setShowDependentsForTodo((curShownDependents) => ({
+      ...curShownDependents,
+      [id]: !curShownDependents[id],
+    }));
+  }
+
+  const mainTodos = () =>
+    currentTodos()
+      .filter((i) => !i.dependsOn)
+      .sort((a, b) => a.createdAt - b.createdAt);
+
+  const getDependentProps = (id?: Todo["id"]): TodoSetProps | undefined => {
+    const dependentTodo = currentTodos().find((i) => i.id === id);
+
+    if (dependentTodo) {
+      return {
+        todoProps: {
+          todo: dependentTodo,
+          class: "my-3",
+          onShowDependentClick: handleShowDependent,
+          onCheck: (checked) =>
+            console.log("the input has been checked =>", checked),
+        },
+        dependentProps: getDependentProps(dependentTodo.dependent),
+        showDependent: showDependentsForTodo()[dependentTodo.id],
+      };
+    }
+  };
+
+  const availableOptions = () => currentTodos().filter((i) => !i.dependent);
+
   return (
-    <div class="flex flex-col items-center">
-      <For each={currentTodos()}>
-        {(todo) => (
-          <TodoComp
-            class="my-3"
-            todo={todo}
-            onCheck={(checked) =>
-              console.log("the input has been checked =>", checked)
-            }
-          />
-        )}
-      </For>
-      <form
-        class="flex flex-col gap-3 items-center border border-red-500"
-        on:submit={handleSubmit}
-      >
+    <div class="flex flex-col h-screen max-h-screen border-2 border-purple-500">
+      <div class="flex justify-center gap-2 border border-green-400">
+        <For each={mainTodos()}>
+          {(todo) => (
+            <div class="border border-red-300 p-2">
+              <TodoSet
+                todoProps={{
+                  todo,
+                  class: "my-3",
+                  onShowDependentClick: handleShowDependent,
+                  onCheck: (checked) =>
+                    console.log("the input has been checked =>", checked),
+                }}
+                dependentProps={getDependentProps(todo.dependent)}
+                showDependent={showDependentsForTodo()[todo.id]}
+              />
+            </div>
+          )}
+        </For>
+      </div>
+      <form class="flex flex-col gap-3 items-center" on:submit={handleSubmit}>
         <input
           type="text"
           placeholder="New TODO"
@@ -54,7 +109,7 @@ export function App() {
           value={dependsOn()}
         >
           <option value=""></option>
-          <For each={currentTodos()}>
+          <For each={availableOptions()}>
             {(todo) => <option value={todo.id}>{todo.description}</option>}
           </For>
         </select>
