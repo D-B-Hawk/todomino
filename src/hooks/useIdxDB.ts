44d5de1650282f7d36d22ba1d7bd244f778db4f4
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-05-27 16:51:17 -0700

    add updateItem method. pass errors to handlers to either store that as a db issue or request issue.

--- src/hooks/useIdxDB.ts
+++ src/hooks/useIdxDB.ts
@@ -2,25 +2,28 @@ import { createSignal, type Accessor } from "solid-js";
 
 type UseIdxDBReturn<Item> = [
   Accessor<Item[]>,
-  Accessor<string | undefined>,
-  { addItem: (item: Item) => void },
+  {
+    addItem: (item: Item) => void;
+    updateItem: (item: Item) => void;
+    reqError: Accessor<string | undefined>;
+    dbError: Accessor<string | undefined>;
+  },
 ];
 
 export function useIdxDB<Item>(dbName: string): UseIdxDBReturn<Item> {
   const [db, setDB] = createSignal<IDBDatabase>();
   const [dbItems, setDBItems] = createSignal<Item[]>([]);
   const [dbError, setDBError] = createSignal<string>();
+  const [reqError, setReqError] = createSignal<string>();
 
   const openDBRequest = window.indexedDB.open(dbName, 3);
 
   openDBRequest.onerror = () => {
-    if (openDBRequest.error?.message) {
-      setDBError(openDBRequest.error.message);
-    }
+    setDBError(openDBRequest.error?.message);
   };
 
   openDBRequest.onupgradeneeded = () => {
-    openDBRequest.result.createObjectStore(dbName, { autoIncrement: true });
+    openDBRequest.result.createObjectStore(dbName, { keyPath: "id" });
   };
 
   openDBRequest.onsuccess = () => {
@@ -42,11 +45,30 @@ export function useIdxDB<Item>(dbName: string): UseIdxDBReturn<Item> {
     const request = objStore.getAll();
 
     request.onerror = () => {
-      setDBError(request.error?.message || "error getting all items");
+      setReqError(request.error?.message);
     };
 
     request.onsuccess = () => {
       setDBItems(request.result);
+    };
+  }
+
+  function handleReqError(this: IDBRequest<IDBValidKey>) {
+    setReqError(this.error?.message);
+  }
+
+  function updateItem(item: Item) {
+    const objStore = createObjectStore();
+    if (!objStore) {
+      setDBError("no object store");
+      return;
+    }
+
+    const request = objStore.put(item);
+    request.onerror = handleReqError;
+
+    request.onsuccess = () => {
+      getAllItems();
     };
   }
 
@@ -59,10 +81,12 @@ export function useIdxDB<Item>(dbName: string): UseIdxDBReturn<Item> {
 
     const addRequest = objStore.add(item);
 
-    addRequest.onerror = () => {
-      setDBError(addRequest.error?.message || "error adding item");
+    addRequest.onerror = handleReqError;
+
+    addRequest.onsuccess = () => {
+      getAllItems();
     };
   };
 
-  return [dbItems, dbError, { addItem }];
+  return [dbItems, { addItem, updateItem, reqError, dbError }];
 }
