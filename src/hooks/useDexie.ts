3b5a42218df1dbf49d05c41852bdfe3b71a0d7a0
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-04 23:35:43 -0700

    add getDependents func

--- src/hooks/useDexie.ts
+++ src/hooks/useDexie.ts
@@ -18,19 +18,34 @@ export function useDexie() {
   const todos = useObservable(todosObservable, []);
 
   async function addTodo(todo: Todo) {
-    let dependedOn: Todo | undefined;
-    if (todo.dependsOn) {
-      dependedOn = await db.todos.get(todo.dependsOn);
-    }
+    const { dependsOnTodo } = await getDependents(todo);
 
     db.transaction("rw", db.todos, async () => {
       await db.todos.put(todo);
-      if (dependedOn) {
-        await db.todos.put({ ...dependedOn, dependent: todo.id });
+      if (dependsOnTodo) {
+        await db.todos.put({
+          ...dependsOnTodo,
+          dependent: todo.id,
+          updatedAt: Date.now(),
+        });
       }
     }).catch((error) => {
       setError(getError(error, "error creating todo"));
     });
+  }
+
+  async function getDependents(todo: Todo) {
+    // defaulting dependent and depends on to a string as bulkGet is looking for a string
+    const { dependent = "", dependsOn = "" } = todo;
+    const [dependentTodo, dependsOnTodo] = await db.todos.bulkGet([
+      dependent,
+      dependsOn,
+    ]);
+
+    return {
+      dependentTodo,
+      dependsOnTodo,
+    };
   }
 
   return [lists, todos, { error, addTodo }] as const;
