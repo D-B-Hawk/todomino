1e739a173620862634c960341ace77dfd772a9e1
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-04 19:49:25 -0700

    add addTodo func

--- src/hooks/useDexie.ts
+++ src/hooks/useDexie.ts
@@ -1,8 +1,13 @@
 import { db } from "../db";
 import { liveQuery } from "dexie";
 import { useObservable } from "./useObservable";
+import type { Todo } from "../types";
+import { createSignal } from "solid-js";
+import { getError } from "../helpers/getError";
 
 export function useDexie() {
+  const [error, setError] = createSignal<Error>();
+
   const listsObservable = liveQuery(async () =>
     (await db.lists.toArray()).map((item) => item.name),
   );
@@ -12,5 +17,21 @@ export function useDexie() {
   const lists = useObservable(listsObservable, []);
   const todos = useObservable(todosObservable, []);
 
-  return [lists, todos] as const;
+  async function addTodo(todo: Todo) {
+    let dependedOn: Todo | undefined;
+    if (todo.dependsOn) {
+      dependedOn = await db.todos.get(todo.dependsOn);
+    }
+
+    db.transaction("rw", db.todos, async () => {
+      await db.todos.put(todo);
+      if (dependedOn) {
+        await db.todos.put({ ...dependedOn, dependent: todo.id });
+      }
+    }).catch((error) => {
+      setError(getError(error, "error creating todo"));
+    });
+  }
+
+  return [lists, todos, { error, addTodo }] as const;
 }
