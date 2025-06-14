54d17f35b1f8db572a367bac241f4a807d9c897d
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-14 17:17:48 +0000

    remove browser testing. will mock dexie for further unit tests

--- src/vitest-example/HelloWorld.browser.test.tsx
+++ src/vitest-example/HelloWorld.browser.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render } from "@solidjs/testing-library";
 import { HelloWorld } from "./HelloWorld.tsx";
 
-test("renders name", async () => {
+test("renders name", () => {
   const { getByText } = render(() => <HelloWorld name="Vitest" />);
   expect(getByText("Hello Vitest!")).toBeTruthy();
 });
