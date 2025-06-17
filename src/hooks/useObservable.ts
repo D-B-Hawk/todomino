e93cdb891a14e655c1ed1ac372311231f86e9817
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-17 18:24:41 +0000

    can pull out use of accessor for observable since i no longer need to change the subscription due to a change outside of db state

--- src/hooks/useObservable.ts
+++ src/hooks/useObservable.ts
@@ -1,18 +1,9 @@
-import { createEffect, createSignal, onCleanup, type Accessor } from "solid-js";
+import { createSignal, onCleanup } from "solid-js";
 import type { Observable } from "dexie";
 
-export function useObservable<T>(
-  observable: Accessor<Observable<T>>,
-  initialValue: T,
-) {
+export function useObservable<T>(observable: Observable<T>, initialValue: T) {
   const [value, setValue] = createSignal(initialValue);
-  let subscription = observable().subscribe(setValue);
-
-  createEffect(() => {
-    // reset the subscription when the observable changes
-    subscription.unsubscribe();
-    subscription = observable().subscribe(setValue);
-  });
+  const subscription = observable.subscribe(setValue);
 
   onCleanup(() => subscription.unsubscribe());
   return value;
