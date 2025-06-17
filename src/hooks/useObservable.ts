b380941e5f3ac9b8911cce4192436fac2c10e0bd
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-17 01:24:26 +0000

    updated useObservable because i needed the query to change do to outside factors like picking the list and showing the todos in that list. the liveQuery only triggers on db changes not outside factors. so if i pass along an accessor for the observable i can reset the subscription at that point

--- src/hooks/useObservable.ts
+++ src/hooks/useObservable.ts
@@ -1,9 +1,19 @@
-import { createSignal, onCleanup } from "solid-js";
+import { createEffect, createSignal, onCleanup, type Accessor } from "solid-js";
 import type { Observable } from "dexie";
 
-export function useObservable<T>(observable: Observable<T>, initialValue: T) {
+export function useObservable<T>(
+  observable: Accessor<Observable<T>>,
+  initialValue: T,
+) {
   const [value, setValue] = createSignal(initialValue);
-  const subscription = observable.subscribe(setValue);
+  let subscription = observable().subscribe(setValue);
+
+  createEffect(() => {
+    // reset the subscription when the observable changes
+    subscription.unsubscribe();
+    subscription = observable().subscribe(setValue);
+  });
+
   onCleanup(() => subscription.unsubscribe());
   return value;
 }
