3f841c683674185b2314ce441e2bc9e954567a09
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-26 22:09:01 +0000

    add IconButton comp

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,20 @@
+import { splitProps, type JSX } from "solid-js";
+import { twMerge } from "tailwind-merge";
+import { Icon, type IconProps } from "./Icon";
+
+interface IconButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
+  iconProps: IconProps;
+}
+
+export function IconButton(props: IconButtonProps) {
+  const [local, buttonProps] = splitProps(props, ["iconProps", "class"]);
+  return (
+    <button
+      class={twMerge("border p-2 rounded-full", local.class)}
+      type="button"
+      {...buttonProps}
+    >
+      <Icon {...local.iconProps} />
+    </button>
+  );
+}
