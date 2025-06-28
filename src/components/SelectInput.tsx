1964385d09368516e99bcc765d6768a05de44400
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-06-28 17:53:51 +0000

    rename type for better readability when in use outside comp

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,17 +1,18 @@
 import { For, Show, type JSX } from "solid-js";
 import { twMerge } from "tailwind-merge";
 
-export type Option = { id: string | number; value: string };
+export type SelectOption = { id: string | number; value: string };
 
-interface SelectInputProps extends JSX.SelectHTMLAttributes<HTMLSelectElement> {
-  options: Option[]; // for whatever reason options is not listed as an attribute in JSX.SelectHTMLAttributes
+export interface SelectInputProps
+  extends JSX.SelectHTMLAttributes<HTMLSelectElement> {
+  options: SelectOption[]; // for whatever reason options is not listed as an attribute in JSX.SelectHTMLAttributes
   withBlankOption?: boolean;
 }
 export function SelectInput(props: SelectInputProps) {
   return (
     <select class={twMerge("rounded-sm px-4 py-2", props.class)} {...props}>
       <Show when={props.withBlankOption}>
-        <option value=""></option>
+        <option></option>
       </Show>
       <For each={props.options}>
         {(option) => <option value={option.id}>{option.value}</option>}
