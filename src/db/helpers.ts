b8c7be1e1a1f4f3b14a24e1e92a757c8ee9ebf52
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-07-03 18:49:41 +0000

    add helper funcs to enfore key type for or clause, as well is table name within transaction

--- src/db/helpers.ts
+++ src/db/helpers.ts
@@ -1,6 +1,6 @@
-import type { Collection, InsertType } from "dexie";
+import type { Collection, InsertType, Table, Transaction } from "dexie";
 import type { Todo } from "../types";
-import { db } from "./db";
+import { db, type TodosDBTable } from "./db";
 
 export function getTodosWhereKey<K extends keyof Todo | Array<keyof Todo>>(
   key: K,
@@ -8,9 +8,23 @@ export function getTodosWhereKey<K extends keyof Todo | Array<keyof Todo>>(
   return db.todos.where(key);
 }
 
+export function orWhereIndexOrPrimary<K extends keyof Todo>(
+  key: K,
+  todos: Collection<Todo, string, InsertType<Todo, "id">>,
+) {
+  return todos.or(key);
+}
+
 export function sortTodosByKey<K extends keyof Todo>(
   key: K,
   todos: Collection<Todo, string, InsertType<Todo, "id">>,
 ) {
   return todos.sortBy(key);
+}
+
+export function typedTable<
+  K extends keyof TodosDBTable,
+  T = TodosDBTable[K] extends Table<infer Row> ? Row : never,
+>(tableName: K, tx: Transaction): Table<T> {
+  return tx.table(tableName) as Table<T>;
 }
