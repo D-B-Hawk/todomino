48ed89bd749cb64b61d15ea2be6db57728ab1fff
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-07-07 16:07:09 +0000

    update tailiwindcss to 4.1. use tailwindcss/postcss in postcssconfig and remove autoprefixer. use dedicated tailwindcss plugin in vite config. remove tailwindcss directives and import tailwindcss in index.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [solid(), solidSvg()],
+  plugins: [tailwindcss(), solid(), solidSvg()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
