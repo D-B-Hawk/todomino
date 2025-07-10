5b02cccb03793200b977e9d581a2be77421b6a34
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-07-10 21:21:25 +0000

    function name change for todos by list name. keep it to doing one thing instead of adding additional args that have caused a little bit of refactoring for that change. add seperate func for seperating complete and incomplete todos.

--- src/context/Dexie/helpers.ts
+++ src/context/Dexie/helpers.ts
@@ -1,24 +1,41 @@
-import { db, getTodosWhereKey, orWhereIndexOrPrimary } from "@/db";
+import {
+  db,
+  getTodosWhereKey,
+  orWhereIndexOrPrimary,
+  sortTodosByKey,
+} from "@/db";
 import { type ListName, type Todo } from "@/types";
+import type { Collection, InsertType } from "dexie";
 
-export function getTodosByListName(
-  listName: ListName,
-  completionType: "removeCompleted" | "keepCompleted" = "removeCompleted",
-) {
+export function getTodosCollectionByListName(listName: ListName) {
   if (listName === "completed") {
     return getTodosWhereKey("completedAt").above(0);
   }
   if (listName === "todomino") {
-    const withDependent = getTodosWhereKey("dependent").notEqual("");
-
-    return orWhereIndexOrPrimary("dependsOn", withDependent).notEqual("");
+    const todos = getTodosWhereKey("dependent").notEqual("");
+    return orWhereIndexOrPrimary("dependsOn", todos).notEqual("");
   }
 
-  let todos = getTodosWhereKey("list").equals(listName);
-  if (completionType === "removeCompleted") {
-    todos = todos.and((todo) => !todo.completedAt);
-  }
-  return todos;
+  return getTodosWhereKey("list").equals(listName);
+}
+
+export async function getCompleteIncompleteTodos(
+  todos: Collection<Todo, string, InsertType<Todo, "id">>,
+) {
+  const complete: Todo[] = [];
+  const incomplete: Todo[] = [];
+
+  const todoArray = await sortTodosByKey("createdAt", todos);
+
+  todoArray.forEach((todo) => {
+    if (todo.completedAt) {
+      complete.push(todo);
+      return;
+    }
+    incomplete.push(todo);
+  });
+
+  return { complete, incomplete };
 }
 
 export async function getDependents(todo: Todo) {
