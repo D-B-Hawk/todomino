d08dc34084da81100a12a7191142eaec9bca4579
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-07-12 18:36:35 +0000

    add OnClickOutside comp

---
+++ src/components/OnClickOutsideContainer.tsx
@@ -0,0 +1,22 @@
+import { createEffect, createSignal, splitProps, type JSX } from "solid-js";
+import { useOnClickOutside } from "@/hooks";
+
+interface OnClickOutsideContainerProps
+  extends Omit<JSX.HTMLAttributes<HTMLDivElement>, "ref"> {
+  onClickOutside: () => void;
+  onDivMount: (div: HTMLDivElement | undefined) => void;
+}
+
+export function OnClickOutsideContainer(props: OnClickOutsideContainerProps) {
+  const [local, rest] = splitProps(props, ["onClickOutside", "onDivMount"]);
+
+  const [divRef, setDivRef] = createSignal<HTMLDivElement>();
+
+  createEffect(() => {
+    local.onDivMount(divRef());
+  });
+
+  useOnClickOutside(divRef, local.onClickOutside);
+
+  return <div ref={setDivRef} {...rest} />;
+}
