af368aa24d8ea46e2ad2b941b128d7d5c15c1ab7
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-07-31 18:48:01 +0000

    update: add ability to update current selected todo. add ability to create a new todo

--- src/views/TodosView/TodosView.tsx
+++ src/views/TodosView/TodosView.tsx
@@ -1,15 +1,55 @@
-import { createEffect, For, Show } from "solid-js";
+import { createEffect, createSignal, For, Show } from "solid-js";
+import { Transition } from "solid-transition-group";
 import type { Todo } from "@/types";
 import { useDexieCtx } from "@/context";
 import { useAsyncDebounce, useToggle } from "@/hooks";
-import { isReadOnlyListName } from "@/helpers";
-import { TodoComp, TodoForm, ScrollableContainer } from "@/components";
+import { createTodo } from "@/helpers";
+import {
+  TodoComp,
+  ScrollableContainer,
+  IconButton,
+  OnClickOutsideContainer,
+} from "@/components";
 import { TodosViewHeader } from "./TodosViewHeader";
+import "./todosView.css";
+import { db } from "@/db";
 
 export function TodosView() {
-  const [{ chosenListTodos, chosenList }, { handleTodoCheck, deleteTodo }] =
-    useDexieCtx();
+  const [
+    { chosenListTodos, chosenList },
+    { handleTodoCheck, addTodo, deleteTodo, updateTodo },
+  ] = useDexieCtx();
   const [showComplete, { toggle }, setShowComplete] = useToggle();
+  const [editedTodo, setEditedTodo] = createSignal<Todo>();
+
+  const [transitonName, setTranstionName] = createSignal("slide-fade");
+
+  const [newTodo, setNewTodo] = createSignal<Todo>();
+
+  async function handleClickOutside() {
+    const edited = editedTodo();
+    if (!edited) return;
+
+    const currentTodo = await db.todos.get(edited.id);
+    if (!currentTodo) return;
+
+    if (
+      edited.description !== currentTodo.description ||
+      edited.dueDate !== currentTodo.dueDate
+    ) {
+      const updatedTodo: Todo = {
+        ...edited,
+        updatedAt: Date.now(),
+        description: edited.description || "New reminder", // give default,
+      };
+
+      updateTodo(updatedTodo).catch((error) => {
+        console.error(error);
+      });
+
+      setEditedTodo(undefined);
+    }
+  }
 
   async function handleCheck(checked: boolean, todo: Todo) {
     return handleTodoCheck(checked, todo).catch((error) =>
@@ -38,6 +78,23 @@ export function TodosView() {
     return incomplete;
   };
 
+  function handleUpdateTodo() {
+    const freshTodo = newTodo();
+    if (freshTodo) {
+      const defaultTodo = createTodo();
+      if (
+        freshTodo.description !== defaultTodo.description ||
+        freshTodo.dueDate !== defaultTodo.dueDate
+      ) {
+        setTranstionName("add-todo");
+        addTodo(freshTodo)
+          .catch((error) => console.error(error))
+          .finally(() => setTranstionName("slide-fade"));
+      }
+    }
+    setNewTodo(undefined);
+  }
+
   return (
     <div class="flex flex-col w-full">
       <Show when={chosenList()}>
@@ -50,19 +107,67 @@ export function TodosView() {
           />
         )}
       </Show>
-      <ScrollableContainer class="p-4 gap-2">
+      <ScrollableContainer class="relative p-4 gap-2">
         <For each={todos()}>
           {(todo) => (
             <TodoComp
               todo={todo}
               onCheck={(checked) => debouncedCheck(checked, todo)}
               onDelete={() => deleteTodo(todo)}
+              onEnter={handleClickOutside}
+              onUpdateDescription={(description) => {
+                setEditedTodo({
+                  ...todo,
+                  description,
+                });
+              }}
+              onUpdateDueDate={(dueDate) => {
+                setEditedTodo({
+                  ...todo,
+                  dueDate,
+                });
+              }}
             />
           )}
         </For>
-        <Show when={!isReadOnlyListName(chosenList()?.name)}>
-          <TodoForm class="w-fit border p-4 px-6 rounded-2xl" />
-        </Show>
+        <Transition name={transitonName()}>
+          <Show when={newTodo()}>
+            {(todo) => (
+              <OnClickOutsideContainer onClickOutside={handleUpdateTodo}>
+                <TodoComp
+                  class="border border-red-300"
+                  todo={todo()}
+                  onCheck={(checked) =>
+                    setNewTodo(() => ({
+                      ...todo(),
+                      completedAt: checked ? Date.now() : undefined,
+                    }))
+                  }
+                  onDelete={() => setNewTodo()}
+                  onEnter={handleUpdateTodo}
+                  onUpdateDescription={(description) =>
+                    setNewTodo(() => ({ ...todo(), description }))
+                  }
+                  onUpdateDueDate={(dueDate) =>
+                    setNewTodo(() => ({ ...todo(), dueDate }))
+                  }
+                />
+              </OnClickOutsideContainer>
+            )}
+          </Show>
+        </Transition>
+        <IconButton
+          onClick={() => {
+            if (newTodo()) {
+              return;
+            }
+            setNewTodo(createTodo());
+          }}
+          class="flex gap-2 mt-auto"
+          iconProps={{ icon: "PLUS_CIRCLE", class: "w-6" }}
+        >
+          Add Todo
+        </IconButton>
       </ScrollableContainer>
     </div>
   );
