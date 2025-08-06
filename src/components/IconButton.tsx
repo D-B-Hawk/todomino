f62d19c660ec917683d40870c309dcfbdef85f0d
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-08-06 21:10:02 +0000

    add: ListPicker comp

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,7 +2,8 @@ import { splitProps, type JSX } from "solid-js";
 import { twMerge } from "tailwind-merge";
 import { Icon, type IconProps } from "./Icon";
 
-interface IconButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface IconButtonProps
+  extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
   iconProps: IconProps;
 }
 
@@ -10,7 +11,7 @@ export function IconButton(props: IconButtonProps) {
   const [local, buttonProps] = splitProps(props, ["iconProps", "class"]);
   return (
     <button
-      class={twMerge("rounded-full cursor-pointer", local.class)}
+      class={twMerge("flex rounded-full cursor-pointer", local.class)}
       type="button"
       {...buttonProps}
     >
