d9078d6b5433d3aa9b6b0e8d8020047b4699005f
Derrick Hawkins <derrick.b.hawkins@gmail.com>
2025-08-21 23:45:23 +0000

    only show the list picker if there is an option outside of the initial lists

--- src/components/ListPicker.tsx
+++ src/components/ListPicker.tsx
@@ -1,9 +1,10 @@
-import { For } from "solid-js";
+import { For, Show } from "solid-js";
 import { useDexieCtx } from "@/context";
 import { isReadOnlyListName } from "@/helpers";
 import { PopUpMenu } from "./PopUpMenu";
 import type { ListName } from "@/types";
 import { PICKER_COLORS } from "@/constants/colors";
+import { INITIAL_LIST_NAMES } from "@/constants/lists";
 
 type ListPickerProps = {
   todoListName: ListName;
@@ -19,37 +20,41 @@ export function ListPicker(props: ListPickerProps) {
         !isReadOnlyListName(list.name) && list.name !== props.todoListName,
     );
 
+  const showListPicker = () => lists().length > INITIAL_LIST_NAMES.length;
+
   return (
-    <PopUpMenu
-      class="p-1 rounded-md h-full"
-      clickOutsideContainerClass="left-1 top-10"
-      iconButtonProps={{
-        iconProps: {
-          icon: "TRELLO",
-        },
-        children: props.todoListName,
-        class: "bg-black/5 hover:bg-black/15",
-      }}
-    >
-      <menu class="flex flex-col gap-1">
-        <For each={listOptions()}>
-          {(list) => (
-            <li>
-              <button
-                type="button"
-                class="flex flex-1 p-2 text-white rounded-md text-nowrap cursor-pointer"
-                style={{ "background-color": PICKER_COLORS[list.color] }}
-                onClick={(e) => {
-                  e.stopImmediatePropagation();
-                  props.onListOptionClick(list.name);
-                }}
-              >
-                {list.name}
-              </button>
-            </li>
-          )}
-        </For>
-      </menu>
-    </PopUpMenu>
+    <Show when={showListPicker()}>
+      <PopUpMenu
+        class="p-1 rounded-md h-full"
+        clickOutsideContainerClass="left-1 top-10"
+        iconButtonProps={{
+          iconProps: {
+            icon: "TRELLO",
+          },
+          children: props.todoListName,
+          class: "bg-black/5 hover:bg-black/15",
+        }}
+      >
+        <menu class="flex flex-col gap-1">
+          <For each={listOptions()}>
+            {(list) => (
+              <li>
+                <button
+                  type="button"
+                  class="flex flex-1 p-2 text-white rounded-md text-nowrap cursor-pointer"
+                  style={{ "background-color": PICKER_COLORS[list.color] }}
+                  onClick={(e) => {
+                    e.stopImmediatePropagation();
+                    props.onListOptionClick(list.name);
+                  }}
+                >
+                  {list.name}
+                </button>
+              </li>
+            )}
+          </For>
+        </menu>
+      </PopUpMenu>
+    </Show>
   );
 }
